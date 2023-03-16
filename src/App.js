7ca4428e3525e5f522b418f58abc7963bcd89dd5
Matt Holt <jamesmattholt@gmail.com>
2023-03-16 19:04:28 +0000

    Aborted dynamic video loading, almost finalised initial layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import PatchesVideo from './assets/videos/tfa-patches-video_HB.mp4';
+
 function App() {
   return (
     <>
@@ -48,6 +50,12 @@ function App() {
               height="150px"
               alt="photo-matt"
             />
+            <img
+              id="photo-matt-inv"
+              src={require('./assets/close-me-bw.jpg')}
+              height="150px"
+              alt="photo-matt"
+            />
           </li>
           <li id="skills-icon">
             <img
@@ -60,30 +68,54 @@ function App() {
       </div>
 
       <h3 id="projects-title">Projects</h3>
-      <h2 id="project-title">That French Artist</h2>
-      <span>
-        <img
-          src={require('./assets/info.png')}
-          height="20px"
-          id="info-icon"
-          alt="info-icon"
-        />
-      </span>
 
-      <div className="video-container">
-        <video controls>
-          <source
-            src={require('./assets/videos/test_TFA_HB.mp4')}
-            type="video/mp4"
-          />
-          Sorry, your browser doesn't support videos.
-        </video>
-        <div>
-          <h5>Choose Section</h5>
-          <div className="sections-container">
-            <div className="tfa-section">1</div>
-            <div className="tfa-section">2</div>
-            <div className="tfa-section">3</div>
+      <div className="project-container">
+        <div className="project-title-container">
+          <h2 id="project-title">That French Artist</h2>
+          <span>
+            <img
+              src={require('./assets/info.png')}
+              height="20px"
+              id="info-icon"
+              alt="info-icon"
+            />
+          </span>
+        </div>
+
+        <div className="video-container">
+          <video controls>
+            <source src={PatchesVideo} type="video/mp4" />
+            Sorry, your browser doesn't support videos.
+          </video>
+          <div className="repo-links-container">
+            <div className="repo-links">
+              <div className="repo-link">
+                <a href="https://mattvwaves.github.io/matt-holt-portfolio.github.io/">
+                  client
+                </a>
+                <a href="https://github.com/MattVwaves">
+                  <img
+                    src={require('./assets/github.png')}
+                    height="35px"
+                    alt="github-icon"
+                  />
+                </a>
+              </div>
+
+              <div className="repo-link">
+                <a href="https://mattvwaves.github.io/matt-holt-portfolio.github.io/">
+                  server
+                </a>
+                <a href="https://github.com/MattVwaves">
+                  <img
+                    src={require('./assets/github.png')}
+                    height="35px"
+                    alt="github-icon"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="repo-links-under"></div>
           </div>
         </div>
       </div>
