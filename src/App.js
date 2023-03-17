86c485b99f60a788d78a24d078880f165be17e3e
Matt Holt <jamesmattholt@gmail.com>
2023-03-17 12:54:40 +0000

    Responsive up to Ipad

--- src/App.js
+++ src/App.js
@@ -15,46 +15,53 @@ function App() {
       <Header />
       <h3 id="projects-title">Projects</h3>
 
-      <div className="project-container">
-        <ProjectTitleContainer info={<TFAInfo />} title="That French Artist" />
-        <div className="video-container">
-          <video controls>
-            <source src={PatchesVideo} type="video/mp4" />
-          </video>
-          <RepoLinks
-            clientUrl="https://github.com/MattVwaves/That-French-Artist-Client"
-            serverUrl="https://github.com/MattVwaves/That-French-Artist-Server"
-          />
-        </div>
-      </div>
+      <div className="outer-projects-container">
+        <div className="projects-container">
+          <div className="project-container" id="that-french-artist">
+            <ProjectTitleContainer
+              info={<TFAInfo />}
+              title="That French Artist"
+            />
+            <div className="video-container">
+              <video controls>
+                <source src={PatchesVideo} type="video/mp4" />
+              </video>
+              <RepoLinks
+                clientUrl="https://github.com/MattVwaves/That-French-Artist-Client"
+                serverUrl="https://github.com/MattVwaves/That-French-Artist-Server"
+              />
+            </div>
+          </div>
 
-      <div className="project-container">
-        <ProjectTitleContainer info={<PSInfo />} title="Pop Shiester" />
-        <div className="video-container">
-          <img
-            width="250px"
-            height="350px"
-            src={require('./assets/pop-sheister-1.png')}
-            alt="pop-shiester-img"
-            id="cover"
-          />
-          <RepoLinks
-            clientUrl="https://github.com/MattVwaves/Pop-Sheister"
-            serverUrl="https://github.com/MattVwaves/Pop-Sheister-server"
-          />
-        </div>
-      </div>
+          <div className="project-container">
+            <ProjectTitleContainer info={<PSInfo />} title="Pop Shiester" />
+            <div className="video-container">
+              <img
+                width="250px"
+                height="350px"
+                src={require('./assets/pop-sheister-1.png')}
+                alt="pop-shiester-img"
+                id="cover"
+              />
+              <RepoLinks
+                clientUrl="https://github.com/MattVwaves/Pop-Sheister"
+                serverUrl="https://github.com/MattVwaves/Pop-Sheister-server"
+              />
+            </div>
+          </div>
 
-      <div className="project-container">
-        <ProjectTitleContainer info={<PFInfo />} title="Portfolio" />
-        <div className="video-container">
-          <img
-            width="250px"
-            height="350px"
-            src={require('./assets/portfolio-grab-2.png')}
-            alt="pop-shiester-img"
-          />
-          <RepoLinks clientUrl="https://github.com/MattVwaves/portfolio-2" />
+          <div className="project-container">
+            <ProjectTitleContainer info={<PFInfo />} title="Portfolio" />
+            <div className="video-container">
+              <img
+                width="250px"
+                height="350px"
+                src={require('./assets/portfolio-grab-2.png')}
+                alt="pop-shiester-img"
+              />
+              <RepoLinks clientUrl="https://github.com/MattVwaves/portfolio-2" />
+            </div>
+          </div>
         </div>
       </div>
     </>
