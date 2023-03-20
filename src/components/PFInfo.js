99cec2025fe13098af03795bc0ceed1af52b0e43
Matt Holt <jamesmattholt@gmail.com>
2023-03-20 12:11:59 +0000

    Chnage video dimensions to % to solve video issues on iphone

--- src/components/PFInfo.js
+++ src/components/PFInfo.js
@@ -1,7 +1,8 @@
 export default function PFInfo() {
   return (
     <p>
-      A tidy, stylish space to curate my projects.
+      A tidy, stylish space to curate my projects... ...with a lil' dash or two
+      of my personality
       <br />
       <br />
       Built with React
